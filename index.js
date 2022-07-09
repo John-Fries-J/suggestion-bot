6cae0bc425e6d90647e4466c2a7c6b6aff3eeb4b
John-Fries-J <w.bababooie@gmail.com>
2022-07-09 16:04:57 +0100

    heroku

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const botclient = require("./bot");
 const config = require("./config.json");
 
@@ -13,7 +14,9 @@ bot.emoji = require('./emojis.js');
 //start the bot
 bot.start();
 
-
+const express = require('express')
+const app = express()
+const port = 3000
 
 
 
